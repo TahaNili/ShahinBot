bcc650e2f8657886e1bc958e59023083426887dd
TahaNili <nilit9330@gmail.com>
2025-05-16 11:09:41 +0330

    Security changes

--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,8 @@
 # config/settings.py
+import os
+from dotenv import load_dotenv
 
-BOT_TOKEN = "7704788538:AAEvlXJpo9ffcPu2ds1I17PrzA-1UaJ2DmI"
-FIREWORKS_API_KEY = "fw_3ZLLahY7SMjXPYxqd2t88YFL"
+load_dotenv()
+
+BOT_TOKEN = os.getenv("7704788538:AAEvlXJpo9ffcPu2ds1I17PrzA-1UaJ2DmI")
+FIREWORKS_API_KEY = os.getenv("fw_3ZLLahY7SMjXPYxqd2t88YFL")
