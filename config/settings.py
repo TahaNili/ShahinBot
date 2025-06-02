99136ee45980d556a06f9425cae130b30bb2c144
TahaNili <nilit9330@gmail.com>
2025-06-02 18:00:50 +0330

    The problem is that due to reasons in all project decision files, we need to roll back the project codes a little bit using Git.

--- config/settings.py
+++ config/settings.py
@@ -4,4 +4,4 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-XAI_API_KEY = os.getenv("XAI_API_KEY")
+FIREWORKS_API_KEY = os.getenv("fw_3ZXwutqSKefqUznA4pUzYddn")
